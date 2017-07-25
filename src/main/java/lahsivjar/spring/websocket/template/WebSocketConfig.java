6080ee69a8450bddaaf5f7eba60830fd955fe4c6
Vishal Raj <lahsivjar@gmail.com>
2017-07-25 07:23:05 +0000

    Initialize spring-websocket

---
+++ src/main/java/lahsivjar/spring/websocket/template/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package lahsivjar.spring.websocket.template;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler(), "/handler");
+    }
+    
+    @Bean
+    public WebSocketHandler webSocketHandler() {
+        return new CustomWebSocketHandler();
+    }
+}
