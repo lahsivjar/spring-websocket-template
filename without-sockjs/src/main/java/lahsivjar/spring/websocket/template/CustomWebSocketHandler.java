3771808b5dc723f6499b644f605a3ae8e15d133c
Vishal Raj <lahsivjar@gmail.com>
2017-08-01 12:42:28 +0800

    refs #1 Fix formatting

--- without-sockjs/src/main/java/lahsivjar/spring/websocket/template/CustomWebSocketHandler.java
+++ without-sockjs/src/main/java/lahsivjar/spring/websocket/template/CustomWebSocketHandler.java
@@ -9,22 +9,23 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class CustomWebSocketHandler extends TextWebSocketHandler {
-	
-	private static final Logger logger = LoggerFactory.getLogger(CustomWebSocketHandler.class);
-    
-	/*
-	 * WebSocket is a very thin layer over TCP and, by default, leaves it upto the application
-	 * to interpret the meaning of message. Since, in this example/template we don't use any
-	 * messaging sub protocol (which is allowed by WebSocket RFC-6455) we need to handle the
-	 * message in its raw form or possibly build a custom framework.
-	 */
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomWebSocketHandler.class);
+
+    /*
+     * WebSocket is a very thin layer over TCP and, by default, leaves it upto
+     * the application to interpret the meaning of message. Since, in this
+     * example/template we don't use any messaging sub protocol (which is
+     * allowed by WebSocket RFC-6455) we need to handle the message in its raw
+     * form or possibly build a custom framework.
+     */
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
-    	try {
-    		TextMessage ackMessage = new TextMessage("ACK: " + message.getPayload());
-    		session.sendMessage(ackMessage);
-    	} catch (IOException e) {
-    		logger.warn("Failed to send message to session {}, ignoring quietly", session.getId());
-    	}
+        try {
+            TextMessage ackMessage = new TextMessage("ACK: " + message.getPayload());
+            session.sendMessage(ackMessage);
+        } catch (IOException e) {
+            logger.warn("Failed to send message to session {}, ignoring quietly", session.getId());
+        }
     }
 }
